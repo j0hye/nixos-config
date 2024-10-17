742cd86e2a7344e01904bfad9a7a8fc808cdfc8d
JW <j0hye@github.com>
2024-10-17 18:24:38 +0000

    fix

--- home/overlays/neovim-nightly.nix
+++ home/overlays/neovim-nightly.nix
@@ -6,6 +6,7 @@ final: prev: {
       owner = "neovim";
       repo = "neovim";
       rev = "nightly";
-      sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # Replace this with the actual hash
+      sha256 = "sha256-RB7vPyiJIjTiKpeeI9wsi2o1H5jsMtWn8Iq1KtjJeDw=";
     };
   });
+}
